d0134521b198ff57f79300f54f4658d76a8e669e
rokey <zenglizz@126.com>
2019-08-27 09:54:02 +0800

    update rpc port

--- slave/cli/main.go
+++ slave/cli/main.go
@@ -16,7 +16,7 @@ import (
 
 func main() {
 	if buildflags.RPCAddr == "" {
-		buildflags.RPCAddr = "http://localhost:8900"
+		buildflags.RPCAddr = "http://localhost:9900"
 	}
 	cli.Run(buildflags.RPCAddr, buildflags.ParaName)
 
